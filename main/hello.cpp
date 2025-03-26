b8121e3720dbf374723ae9d724d700c92fbf91dc
Prithvi-SK <prithvi.sk.0509@gmail.com>
2025-03-26 22:58:45 +0530

    new t4

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout << "task 4";
+  cout << "t 4";
   return 0;
 }
